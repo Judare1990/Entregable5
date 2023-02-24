0a148d4826a9730812a2272047d2747c91ff49e8
Judare85 <judare85@hotmail.com>
2023-02-24 17:24:26 -0500

    Last last Commit

--- src/pages/PokeInfo.jsx
+++ src/pages/PokeInfo.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import Header from '../components/pokedex/Shared/header'
+import Header from '../components/pokedex/Shared/Header'
 import './styles/pokeInfo.css'
 import './styles/loading.css'
 import './styles/returnGoTopButton.css'
