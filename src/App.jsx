5c3ea6dfac91cbb88ddc031178a2565354c7fb99
Judare85 <judare85@hotmail.com>
2023-02-24 03:05:16 -0500

    final

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Pokedex from './pages/Pokedex'
+import ProtectedRoutes from './pages/ProtectedRoutes'
+import PokeInfo from './pages/PokeInfo'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Routes>
+      <Route path='/' element={<Home/>} />
+
+      <Route element= {<ProtectedRoutes/>}>
+      <Route path='/pokedex' element={<Pokedex />} />
+        <Route path= '/pokedex/:id' element={<PokeInfo />} />
+      </Route>
+
+     </Routes>
+    </div>
+  )
+}
+
+export default App
